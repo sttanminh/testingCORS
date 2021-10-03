86f897c61a7c20488fd0d829ef84e5c6ea02091c
Tan Minh Nguyen <st.tanminh@gmail.com>
2021-10-04 04:14:50 +1100

    asd

--- index.js
+++ index.js
@@ -46,12 +46,14 @@ function callApi() {
 }
 
 const port = process.env.PORT || "9000";
+if (process.env.NODE_ENV === "production") {
 server.use('/',
   express.static('client/build')
 );
 server.get('*',(req,res)=>{
   res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
 })
+}
 server.listen(port, () => {
   console.log(`Proxy server listening on http://localhost:${port}`)
 })
