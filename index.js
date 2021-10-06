aae3dbb4f2e2f872d983a9897fcd569506821097
sttanminh <75659577+sttanminh@users.noreply.github.com>
2021-10-06 22:00:17 +1100

    aasd

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const port = process.env.PORT || 9000;
 if (process.env.NODE_ENV === "production") {
 server.use('/static', express.static('client/build'));
 server.get('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'./client/build','index.html'))
+  res.sendFile('./client/build','index.html')
 })
 }
 server.listen(port, () => {
